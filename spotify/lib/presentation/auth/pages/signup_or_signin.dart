bf121ee8a9b354ea601244fb9d07ff03e27edd48
Filip746 <fkvesic@mathos.hr>
2025-02-07 20:58:05 +0100

    completed Login or Signup page and added button for page back

--- spotify/lib/presentation/auth/pages/signup_or_signin.dart
+++ spotify/lib/presentation/auth/pages/signup_or_signin.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify/common/helpers/is_dark_mode.dart';
+import 'package:spotify/common/widgets/appbar/app_bar.dart';
+import 'package:spotify/common/widgets/button/basic_app_button.dart';
 import 'package:spotify/core/configs/assets/app_images.dart';
 import 'package:spotify/core/configs/assets/app_vectors.dart';
+import 'package:spotify/core/configs/theme/app_colors.dart';
 
 class SignupOrSigninPage extends StatelessWidget {
   const SignupOrSigninPage({super.key});
@@ -11,6 +15,7 @@ class SignupOrSigninPage extends StatelessWidget {
     return Scaffold(
       body: Stack(
         children: [
+          BasicAppbar(),
           Align(
             alignment: Alignment.topRight,
             child: SvgPicture.asset(AppVectors.topPattern),
@@ -22,6 +27,58 @@ class SignupOrSigninPage extends StatelessWidget {
           Align(
             alignment: Alignment.bottomLeft,
             child: Image.asset(AppImages.authBG),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(AppVectors.logo),
+                  SizedBox(height: 55),
+                  Text('Enjoy Listening To Music',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 26,
+                      )),
+                  SizedBox(height: 21),
+                  Text(
+                    'Spotify is a proprietary Swedish audio streaming and media services provider ',
+                    style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
+                        color: AppColors.grey),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    children: [
+                      Expanded(
+                          flex: 1,
+                          child: BasicAppButton(
+                              onPressed: () {}, title: 'Register')),
+                      SizedBox(width: 20),
+                      Expanded(
+                        flex: 1,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  color: context.isDarkMode
+                                      ? Colors.white
+                                      : Colors.black),
+                            )),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
           )
         ],
       ),
